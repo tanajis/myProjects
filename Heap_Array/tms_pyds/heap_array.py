072a19aa374408d66c85479180de877c8127e5a9
Tanaji Sutar <tanajisutar31@gmail.com>
2020-03-30 16:54:43 +0530

    Update heap_array.py

--- Heap_Array/tms_pyds/heap_array.py
+++ Heap_Array/tms_pyds/heap_array.py
@@ -52,11 +52,11 @@ class Heap:
 
                 if pos == 1:
                     break
-                #get parent
+                # Get parent
                 parent_index  = math.floor(pos/2)
                 
                 if self.arr[parent_index-1] > self.arr[pos-1]:
-                    #swap
+                    # Swap
                     self.arr[parent_index-1],self.arr[pos-1] = self.arr[pos-1],self.arr[parent_index-1]
                 else:
                     break
@@ -73,16 +73,15 @@ class Heap:
             min_number : (int) 
         """
 
-        #when pop, always remove from top of the three(first element in array)
-        #and put last element at the root.
+        # When pop, always remove from top of the three(first element in array) and put last element at the root.
         
 
         min_number = copy.deepcopy(self.arr[0])
-        #move last element to the root
+        # Move last element to the root
         self.arr[0] = self.arr[-1]
         del self.arr[-1]
 
-        #start from root now
+        # Start from root now
         pos = 1
 
         while(1):
@@ -94,32 +93,32 @@ class Heap:
             if pos == array_length:
                 break
             
-            #if left exist
+            # If left exist
             if left_child_pos <= array_length :
                 
-                #If right exist
+                # If right exist
                 if right_child_pos <=array_length:
                     
-                    #if node is less that its child
+                    # If node is less than its child
                     if self.arr[pos-1] > self.arr[left_child_pos-1] or self.arr[pos-1] > self.arr[right_child_pos-1] :
                         
-                        #swap with smallest one
+                        # Swap with smallest one
                         if self.arr[left_child_pos-1] > self.arr[right_child_pos-1]:
-                            #if right is smaller,swap with right
+                            # If right is smaller,swap with right.
                             self.arr[pos-1],self.arr[right_child_pos-1] = self.arr[right_child_pos-1],self.arr[pos-1]
                             pos = right_child_pos
 
                         else:
-                            #swap with left
+                            # Swap with left
                             self.arr[pos-1],self.arr[left_child_pos-1] = self.arr[left_child_pos-1],self.arr[pos-1]
                             pos = left_child_pos
                 else:
-                    #if right not exist swap with left
+                    # If right not exist swap with left
                     self.arr[pos-1],self.arr[left_child_pos-1] = self.arr[left_child_pos-1],self.arr[pos-1]
                     pos = left_child_pos
 
             else:
-                #left and right both does not exist,means we reached at the end.
+                # The left and right both does not exist,means we reached at the end.
                 break
 
         return min_number
